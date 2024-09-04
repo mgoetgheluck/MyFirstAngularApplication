680603e861781009e1c11d67a39470a810d38ce1
Maxime <mgoetghe@gmail.com>
2024-09-04 10:47:41 +0200

    my first commit

---
+++ snapface/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FaceSnapComponent } from './face-snap/face-snap.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    FaceSnapComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+
+}
---
+++ snapface/src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  standalone: true,
+  imports: [],
+  templateUrl: './face-snap.component.html',
+  styleUrl: './face-snap.component.scss'
+})
+export class FaceSnapComponent {
+  title!: string;
+  description!: string;
+  createdAt!: Date;
+  snaps!: number;
+  imageUrl!: string;
+
+  ngOnInit(): void {
+    this.title = "Archibald";
+    this.description = "Mon meilleur ami";
+    this.createdAt = new Date();
+    this.snaps = 5;
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+  }
+}
