3db13d982b859929e5c78977fb91278219b84692
Maxime <mgoetghe@gmail.com>
2024-09-04 12:04:50 +0200

    commit

---
+++ snapface/src/app/models/face-snap.ts
@@ -0,0 +1,15 @@
+export class FaceSnap {
+    title: string;
+    description: string;
+    createdDate: Date;
+    snaps: number;
+    imageUrl: string;
+
+    constructor(title: string, description: string, imageUrl: string, createdDate: Date, snaps: number){
+        this.title = title;
+        this.description = description;
+        this.createdDate = createdDate;
+        this.snaps = snaps;
+        this.imageUrl = imageUrl;
+    }
+}
