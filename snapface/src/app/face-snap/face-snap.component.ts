27457bc54f2996bead149f5b9d371435364d2ae9
Maxime <mgoetghe@gmail.com>
2024-09-04 11:02:11 +0200

    my first commit on this branch

--- snapface/src/app/face-snap/face-snap.component.ts
+++ snapface/src/app/face-snap/face-snap.component.ts
@@ -13,12 +13,29 @@ export class FaceSnapComponent {
   createdAt!: Date;
   snaps!: number;
   imageUrl!: string;
+  snapButtonText!: string;
+  hasUserSnapped!: boolean;
 
   ngOnInit(): void {
     this.title = "Archibald";
     this.description = "Mon meilleur ami";
     this.createdAt = new Date();
-    this.snaps = 5;
+    this.snaps = 0;
     this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+    this.snapButtonText = "Oh, snap";
+    this.hasUserSnapped = false;
+  }
+
+  onClickSnap(): void{
+    if(this.hasUserSnapped){
+      this.snapButtonText = "Oops, Snap !"
+      this.snaps--;
+      this.hasUserSnapped = false;
+    }
+    else {
+      this.snapButtonText = "Oh, Unsnap !";
+      this.snaps++;
+      this.hasUserSnapped = true;
+    }
   }
 }
