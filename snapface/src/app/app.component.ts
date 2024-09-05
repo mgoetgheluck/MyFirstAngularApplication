0b13d3a63b0a0405e5db9b7f8a039c975d57a46e
Maxime <mgoetghe@gmail.com>
2024-09-05 10:55:04 +0200

    Adding routing

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
 import { HeaderComponent } from './header/header.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    FaceSnapListComponent,
-    HeaderComponent
+    HeaderComponent,
+    RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
